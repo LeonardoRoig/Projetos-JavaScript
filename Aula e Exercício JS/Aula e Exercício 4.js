cd22244eb3301cca3b7930904f91621ef6d4d093
Leonardo C. Lima <leonardocmdo@hotmail.com>
2022-01-28 09:49:06 -0300

    Create Aula e Exercício 4.js

---
+++ Aula e Exercício JS/Aula e Exercício 4.js
@@ -0,0 +1,37 @@
+let numero = 1;
+while(numero <= 10) {
+    console.log(numero);
+    numero++;
+}
+
+
+let contagem = 10;
+while(contagem >= 1) {
+    console.log(contagem);
+    contagem--;
+}
+
+let number = 1;
+while(number >= 10) console.log(number++);
+
+
+
+let numero = 1;
+do {
+    console.log(numero);
+    numero++;
+}while(numero <= 10);
+
+
+
+
+let numero = 1;
+do {
+    console.log(numero);
+    numero++;
+    if(numero > 6) break;
+}while(numero <= 10);
+
+
+
+
